233aa4948a36b9ee32b4ae51b59032003d852a87
刘晓颖 <liuxiaoyingmail@163.com>
2020-11-22 20:26:26 +0800

    添加笛卡尔积方法

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
+<!--
+ * @Author: liuxiaoying
+ * @Date: 2020-11-22 19:47:06
+ * @LastEditors: liuxiaoying
+ * @LastEditTime: 2020-11-22 20:25:38
+ * @Description: README
+-->
 ## README
+
+### Front
+
+#### 项目介绍
+
+整理实际项目开发中使用到的工具方法，逻辑与解决方案，以备不时之需，毕竟业务功能就那么多，需要整理和总结，提高工作效率
+
 
 ### utils.js
 
 + `idToTime`mongodb的id转为时间
 + `checkIsIMG`判断字符串是否为图片地址
 + `reduceArr`数组降维
++ `cartesianArray`数组形式的入参计算笛卡尔积
++ `cartesianObj`对象形式的入参计算笛卡尔积
 
 ### filters.js
 
--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: liuxiaoying
+ * @Date: 2020-11-22 19:47:06
+ * @LastEditors: liuxiaoying
+ * @LastEditTime: 2020-11-22 20:25:50
+ * @Description: 工具方法
+ */
 /**
  * id转为时间
  */
@@ -19,4 +26,53 @@ checkIsIMG(content) {
  */
 reduceArr(arr) {
   return Array.prototype.concat.apply([], arr)
+}
+
+/**
+ * 笛卡尔积-数组形式SKU计算
+ * let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+  ]
+ */
+
+cartesianArray(arr) {
+  return arr.reduce((acc, curr) => {
+    var result = []
+    acc.forEach((item)=> {
+      curr.forEach((currItem) => {
+        result.push([...item, ...currItem])
+      })
+    })
+  }, [[]])
+}
+
+/**
+ * 笛卡尔积-对象形式SKU计算
+ * [{
+ *  a: [1, 2, 3]
+ * }, {
+ *  b: [4, 5, 6]
+ * }, {
+ *  c: [7, 8, 9]
+ * }]
+ */
+cartesianObj (array) {
+  function c (part, index) {
+    var k = Object.keys(array[index])[0]
+    array[index][k].forEach(function (a) {
+      var p = Object.assign({}, part, { [k]: a })
+      if (index + 1 === array.length) {
+        r.push(p)
+        return
+      }
+      c(p, index + 1)
+    })
+  }
+
+  var r = []
+  c({}, 0)
+  return r
 }
